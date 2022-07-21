f510e39b6c26555ba377283a78059b9c9ffeaed5
Oscar <theblue154@gmail.com>
2022-07-21 20:44:42 +0200

    Created skeleton

---
+++ src/Frontpage/Frontpage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./frontpage.scss"
+
+export default function Frontpage() {
+  return (
+    <div className="home">
+      <div className="homeContainer">
+        <div className="widgets">
+        </div>
+        <div className="charts">
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+        </div>
+      </div>
+    </div>
+  )
+}
