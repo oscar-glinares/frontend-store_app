a66ad5cb8d79d4aa4b7d7018c31259393cdab772
Oscar <theblue154@gmail.com>
2022-07-22 20:16:12 +0200

    Basic first connection to backend

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Frontpage from './Frontpage/Frontpage';
 import Navbar from './navbar/Navbar';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 
 function App() {
+  const [products, setProducts] = useState({});
+  useEffect(() => {
+    async function getProducts() {
+      const temp = await axios.get(
+        `http://localhost:8080/api/get`
+      )
+      setProducts(temp.data.rows)
+    }
+    getProducts()
+  },[])
+
   return (
     <div className="App">
       <Router>
-        <Navbar />
+        <Navbar />{console.log(products)}
         <Routes>
-          <Route path="/" element={<Frontpage/>} />
+          <Route path="/" element={<Frontpage />} />
         </Routes>
       </Router>
     </div>
